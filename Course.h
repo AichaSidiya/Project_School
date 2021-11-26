615321d44ccbd2cebb6337f51df7fe2bd916ed58
AichaSidiya <91727165+AichaSidiya@users.noreply.github.com>
2021-11-26 13:06:02 +0300

    Add files via upload

---
+++ Course.h
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+class Course{
+private:
+  //attributes
+  int idCourse; //course identifier
+  string subject; //course subject
+  int code; //course code
+  char section; //course section
+  string term; //course term
+  string instructor; //course instructor
+
+public:
+  //methods
+  Course(); //default constructor
+  Course(int, string, int, char, string, string); //constructor
+  void set(int, string, int, char, string, string); //setter
+  int getIdCourse(); //getter id
+  string getCourseCode(); //return function prototype that returns the concatenated course code
+  bool lessThan(Course*); //lessThan function prototype
+  void print();//print data
+};
